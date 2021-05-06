cb72c599b716dda908b7529818c94bc24db2fa76
gc0rtes <guilhermeco@gmail.com>
2021-05-06 12:25:02 +0200

    React Route added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { Switch, Route } from "react-router-dom";
+import "./App.css";
+import NavBar from "./components/NavBar";
+import AboutPage from "./pages/AboutPage";
+import DiscoverMoviesPage from "./pages/DiscoverMoviesPage";
+import HomePage from "./pages/HomePage";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <NavBar />
+      <Switch>
+        <Route path="/discover" component={DiscoverMoviesPage} />
+        <Route path="/about" component={AboutPage} />
+        <Route path="/" component={HomePage} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
